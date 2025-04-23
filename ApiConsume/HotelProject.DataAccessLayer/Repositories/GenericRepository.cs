57af6d1e2a449dd8545ec393542123cc8e975348
Kübra Aykol <kubraaykol@outlook.com>
2025-04-23 23:04:56 +0300

    chore: updates generic reposiyories and rebase gitignore files

---
+++ ApiConsume/HotelProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T t)
+        {
+            _context.Remove(t);
+            _context.SaveChanges();
+
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
